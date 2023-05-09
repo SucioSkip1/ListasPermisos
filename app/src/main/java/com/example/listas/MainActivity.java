3c6cfc49d80893ae0085df213fc3a83c498ebd68
SucioSkip1 <Shippudenlaloboy@hottmail.com>
2023-05-09 15:40:25 -0600

    Se cambio el wrap content del constrait layout

--- app/src/main/java/com/example/listas/MainActivity.java
+++ app/src/main/java/com/example/listas/MainActivity.java
@@ -8,12 +8,16 @@ public class MainActivity extends ListActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        /*
       //se elimina por que la lista no esta echa.
         //  setContentView(R.layout.activity_main);
         setListAdapter(new ArrayAdapter<String>(getApplicationContext(),
                                             //id caja de texto
                 R.layout.activity_main,R.id.txtTitulo,
                 // modelo                              //con que arreglo queremos
-                getResources().getStringArray(R.array.Colores)));
+                getResources().getStringArray(R.array.Bandas)));*/
+
+
+
     }
 }
