68bbf9c350d62f406abd32e409d044b6684af761
SucioSkip1 <Shippudenlaloboy@hottmail.com>
2023-05-09 16:03:14 -0600

    Se comento el codigo y por eso no cargaban las imagenes

--- app/src/main/java/com/example/listas/MainActivity.java
+++ app/src/main/java/com/example/listas/MainActivity.java
@@ -8,14 +8,14 @@ public class MainActivity extends ListActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        /*
+
       //se elimina por que la lista no esta echa.
         //  setContentView(R.layout.activity_main);
         setListAdapter(new ArrayAdapter<String>(getApplicationContext(),
                                             //id caja de texto
                 R.layout.activity_main,R.id.txtTitulo,
                 // modelo                              //con que arreglo queremos
-                getResources().getStringArray(R.array.Bandas)));*/
+                getResources().getStringArray(R.array.Bandas)));
 
 
 
